7958c0fd2921f8d9ca10c9f7fde04ab74f52440a
Vanreuth <hengvanreuth.17@gmail.com>
2025-08-16 22:01:35 +0700

    done

--- tf/main.tf
+++ tf/main.tf
@@ -55,9 +55,25 @@ resource "aws_key_pair" "node_key" {
   public_key = var.ec2_ssh_pub_key
 }
 
+# Data source to find the latest Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # EC2 instance
 resource "aws_instance" "node_server" {
-  ami                    = "ami-0c61361d" # Ubuntu 22.04 (update if needed)
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.node_key.key_name
   vpc_security_group_ids = [aws_security_group.node_sg.id]
